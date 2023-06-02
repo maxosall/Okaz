03ba092286c45d8c5162743e6c2fede76caf2572
Maxo166 <sallahmad059@gmail.com>
2023-06-02 17:14:33 +0300

    './**/*.html',

---
+++ Okaz.Client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html",
+            "./**/*.razor"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
